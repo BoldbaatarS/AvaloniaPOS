3bfae5c8a5ab86a30dfd8327bef85641b1791f9a
POS <contact@electromon.com>
2025-08-21 17:50:35 +0800

    Add docker service amd MSSQL

--- CloudApi/Program.cs
+++ CloudApi/Program.cs
@@ -1,22 +1,28 @@
-using Microsoft.EntityFrameworkCore;
 using Shared.Protos;
 using CloudApi;
-
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // HTTP/2
-builder.WebHost.ConfigureKestrel(o => o.ListenAnyIP(5001, lo => lo.Protocols = Microsoft.AspNetCore.Server.Kestrel.Core.HttpProtocols.Http2));
+builder.WebHost.ConfigureKestrel(o =>
+    o.ListenAnyIP(5001, lo => lo.Protocols = Microsoft.AspNetCore.Server.Kestrel.Core.HttpProtocols.Http2));
 
+// Add services
 builder.Services.AddGrpc();
 builder.Services.AddDbContext<CloudDbContext>(o =>
-    o.UseSqlite($"Data Source={Path.Combine(AppContext.BaseDirectory, "cloud.db")}"));
+    o.UseSqlServer(builder.Configuration.GetConnectionString("CloudDb")));
 
 var app = builder.Build();
 
+// DB migrate автоматаар хийх
 using (var scope = app.Services.CreateScope())
-    scope.ServiceProvider.GetRequiredService<CloudDbContext>().Database.Migrate();
+{
+    var db = scope.ServiceProvider.GetRequiredService<CloudDbContext>();
+    db.Database.Migrate();
+}
 
 app.MapGrpcService<SyncGrpcService>();
-app.MapGet("/", () => "gRPC server.");
+app.MapGet("/", () => "gRPC server running with MSSQL");
+
 app.Run();
